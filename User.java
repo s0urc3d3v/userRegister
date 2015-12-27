cd5910b3e53d4934def1423ce63c3b0f6a0e4c47
s0urc3d3v3l0pm3nt <Matthewelbing@gmail.com>
2015-12-27 15:42:33 -0500

    move UI code into UI class

--- User.java
+++ User.java
@@ -1,52 +1,7 @@
-import javax.swing.*;
-import java.awt.*;
-
 /**
  * Created by Matthewelbing on 12/27/15.
  */
 public class User {
-    private JFrame frame;
-
-    private JPanel panel, buttonPanel;
-
-    private JTextField firstName, lastName, age, weight, position;
-
-    private JButton sumbit, exit;
-    public User(){
-        frame = new JFrame("Registration");
-
-        panel = new JPanel();
-        buttonPanel = new JPanel();
-
-        firstName = new JTextField("First Name");
-        lastName = new JTextField("Last Name");
-        age = new JTextField("Age");
-        weight = new JTextField("Weight");
-        position = new JTextField("Position");
-
-        frame.setBounds(800, 800, 0, 0);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-
-        frame.add(panel, BorderLayout.NORTH);
-        frame.add(buttonPanel, BorderLayout.SOUTH);
-
-        panel.add(firstName);
-        panel.add(lastName);
-        panel.add(age);
-        panel.add(weight);
-        panel.add(position);
-
-        buttonPanel.add(sumbit);
-        buttonPanel.add(exit);
-
-        sumbit.addActionListener(e -> {
-            
-        });
-        exit.addActionListener(e -> {
-                System.exit(0);
-        });
-
-
+    public User() {
     }
 }
